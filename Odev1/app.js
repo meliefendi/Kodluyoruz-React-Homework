955bbb2e6404ad0eab0c95180373585becac05cb
Melih Akkas <melh_akkas@hotmail.com>
2023-02-16 17:04:29 +0300

    Update app.js

--- Odev1/app.js
+++ Odev1/app.js
@@ -1,17 +1,38 @@
+// import axios from "axios";
+
+// export async function getData(Number) {
+//     try {
+//         const { data: userId } = await axios("https://jsonplaceholder.typicode.com/users/" + Number);
+//         const { data: posts } = await axios("https://jsonplaceholder.typicode.com/posts/" + Number);
+
+// userId.post = new Array();
+// userId.post.push(posts)
+
+//         return { userId, posts };
+
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+
+
+// export default getData;
+
+
+////////////////////////////////////
+
 import axios from "axios";
 
-export async function getData(Number) {
-    try {
+const getData = async (Number) => {
+    return new Promise (async(resolve, reject)=>{
         const { data: userId } = await axios("https://jsonplaceholder.typicode.com/users/" + Number);
-        const { data: posts } = await axios("https://jsonplaceholder.typicode.com/posts/" + Number)
+         const { data: posts } = await axios("https://jsonplaceholder.typicode.com/posts/" + Number);
 
-        return { userId, posts };
+         userId.post = new Array()
+         userId.post.push(posts)
 
-    } catch (e) {
-        console.log(e)
-    }
+         resolve(userId);
+    })
 }
 
-
 export default getData;
-
