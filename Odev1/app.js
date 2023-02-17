38ef3af9980495eee10f47e8e3db1424e57d97b9
Melih Akkas <melh_akkas@hotmail.com>
2023-02-17 13:52:41 +0300

    Update app.js

--- Odev1/app.js
+++ Odev1/app.js
@@ -17,6 +17,36 @@
 
 
 ////////////////////////////////////
+import axios from "axios"
+
+
+const getUserr =async(_id) => {
+    const {data : user} = await axios("https://jsonplaceholder.typicode.com/users/"+ _id);
+    console.log(user);
+};
+
+
+const getPostt = async(_id) => {
+    const {data : post} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${_id}`);
+    console.log(post); 
+};
+
+
+function getData(_id) {
+
+    try {
+        const user = getUserr(_id);
+        const post = getPostt(_id);
+    }
+
+    catch (e) {
+        return e;
+    }
+
+}
+export default getData;
+
+////////////////////////////////////////////////////////////////////////
 
 import axios from "axios";
 
